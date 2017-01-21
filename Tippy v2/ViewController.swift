92b4e38a708112b415c19ee1c7a117fe1c2c01db
Jason Hahn <boogi@jasons-imac.fios-router.home>
2017-01-21 08:30:42 -0800

    Initial Commit

---
+++ Tippy v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy v2
+//
+//  Created by Jason Hahn on 1/21/17.
+//  Copyright © 2017 Jason Hahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
